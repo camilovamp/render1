4c33e0b8e8d49f87f0cee9efd6154dcf865ce755
Camilo <camilovamp@gmail.com>
2023-10-27 09:57:54 -0400

    adding headers

--- app.py
+++ app.py
@@ -2,3 +2,9 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 
+
+st.header('Hello Mike')
+st.write("""
+##### Filter the data below to see the ads by manufacturer
+""")
+
